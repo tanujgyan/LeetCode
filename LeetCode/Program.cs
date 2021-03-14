84e685bd15ea61e5cd43a0f00995f5abe903bb42
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-03-13 23:25:41 -0500

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSumDriverCode();
+        }
+        static void TwoSumDriverCode()
+        {
+            TwoSum twoSum = new TwoSum();
+            int[] nums = new int[] { 3,4,-3,90 };
+            int target = 0;
+            var result = twoSum.TwoSumCalculator(nums, target);
+            var dictResult = twoSum.TwoSumCalculatorUsingDictionary(nums, target);
+            result.ToList().ForEach(x => Console.WriteLine(x));
+            dictResult.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class TwoSum
+    {
+        /// <summary>
+        /// This is a brute force method and has a time complexity of O(n2)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSumCalculator(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+            return result;
+        }
+        public int[] TwoSumCalculatorUsingDictionary(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i] ))
+                {
+                    result[0] = dict[target - nums[i]];
+                    result[1] = i;
+                    break;
+                }
+                dict.Add(nums[i], i);
+            }
+            return result;
+        }
+    }
+}
