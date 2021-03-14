1a77b44f84032286524645686ff414741c2e472e
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-03-14 00:18:50 -0500

    Reverse Integer

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,17 +7,24 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            TwoSumDriverCode();
+            //TwoSumDriverCode();
+            ReverseIntegerDriverCode();
         }
         static void TwoSumDriverCode()
         {
             TwoSum twoSum = new TwoSum();
-            int[] nums = new int[] { 3,4,-3,90 };
+            int[] nums = new int[] { 3, 4, -3, 90 };
             int target = 0;
             var result = twoSum.TwoSumCalculator(nums, target);
             var dictResult = twoSum.TwoSumCalculatorUsingDictionary(nums, target);
             result.ToList().ForEach(x => Console.WriteLine(x));
             dictResult.ToList().ForEach(x => Console.WriteLine(x));
+        }
+        static void ReverseIntegerDriverCode()
+        {
+            ReverseInteger reverseInteger = new ReverseInteger();
+            Console.WriteLine(reverseInteger.ReverseIntegerUsingStringConversion((int)Math.Pow(2,33)));
+            Console.WriteLine(reverseInteger.ReverseIntegerUsingStringConversionNoLINQ(-321));
         }
     }
 }
---
+++ LeetCode/ReverseInteger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// This code takes 96 ms
+    /// It uses the reverse function of Linq to revert the string and then using try parse gets the result or 0
+    /// </summary>
+    class ReverseInteger
+    {
+        public int ReverseIntegerUsingStringConversion(int x)
+        {
+            string positive = "";
+            string negative = "";
+            int result = 0;
+            if (x>= 0)
+            {
+                positive =String.Join("", x.ToString().Reverse());
+                result = Int32.TryParse(positive, out result) == false ? 0 : result;
+            }
+            else if (x<0)
+            {
+                negative = "-" + String.Join("",x.ToString().Substring(1).Reverse());
+                result = Int32.TryParse(negative, out result) == false ? 0 : result;
+            }
+            return result;
+            
+        }
+
+        /// <summary>
+        /// This code takes 48 ms 
+        /// It uses for loop revert the string and then using try parse gets the result or 0
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public int ReverseIntegerUsingStringConversionNoLINQ(int x)
+        {
+            string positive="",tempPositive = "";
+            string negative="",tempNegative = "";
+            int result = 0;
+            if (x >= 0)
+            {
+                positive = x.ToString();
+                for(int i=positive.Length-1;i>=0;i--)
+                {
+                    tempPositive = tempPositive + positive[i];
+                }
+                result = Int32.TryParse(tempPositive, out result) == false ? 0 : result;
+            }
+            else if (x < 0)
+            {
+                negative =  x.ToString().Substring(1);
+                for (int i = negative.Length - 1; i >= 0; i--)
+                {
+                    tempNegative = tempNegative + negative[i];
+                }
+                result = Int32.TryParse("-"+tempNegative, out result) == false ? 0 : result;
+            }
+            return result;
+
+        }
+    }
+}
