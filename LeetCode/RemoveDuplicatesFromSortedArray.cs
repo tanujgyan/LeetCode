7de8d38e45d43540bb78943319a869c0fc731c97
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-03-23 22:16:51 -0400

    Added summary for method

--- LeetCode/RemoveDuplicatesFromSortedArray.cs
+++ LeetCode/RemoveDuplicatesFromSortedArray.cs
@@ -5,8 +5,18 @@ using System.Linq;
 
 namespace LeetCode
 {
+
     public class RemoveDuplicatesFromSortedArray
     {
+        /// <summary>
+        /// The idea is to take two pointers i and j.
+        /// Initialize j with i+1. this is where we start looping
+        /// keep increasing j by 1 till we find a num[j] which is not equal to num[i]. Once found
+        /// copy num[j] to num[i+1] and move the pointer i to next number. 
+        /// Keep increasing j till we reach another mismatch and repeat the process.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int RemoveDuplicates(int[] nums)
         {
             int j = 0, i = 0, sum = 0;
