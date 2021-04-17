b1a3612e673766c50b24ecc132c49d1cb02a812b
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-04-17 17:12:36 -0400

    https://leetcode.com/discuss/interview-question/351783/ https://leetcode.com/problems/number-of-good-pairs/

---
+++ LeetCode/NumberOfGoodPairs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NumberOfGoodPairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+            int goodPairs = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+                if(!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], new List<int> { i });
+                }
+                else
+                {
+                    dict[nums[i]].Add(i);
+                }
+            }
+            foreach(var element in dict)
+            {
+                if (element.Value.Count > 1)
+                {
+
+                    //int f1 = Factorial(element.Value.Count);
+                    //int f2 = Factorial(element.Value.Count - 2);
+
+                   goodPairs = goodPairs + (int)(Factorial(element.Value.Count) / (2 * Factorial(element.Value.Count - 2)));
+
+                   // goodPairs = goodPairs + ((element.Value.Count) * (element.Value.Count - 1) / 2);
+                }
+            }
+            return goodPairs;
+        }
+        public double Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCode.Microsoft_Interview_Questions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -38,7 +39,22 @@ namespace LeetCode
             //MajorityElementClassDriverCode();
             //RemoveAllAdjacentDuplicatesInString2DriverCode();
            // ContainsDuplicateClassDriverCode();
-            MinDeletionUniqueStringDriverCode();
+            //MinDeletionUniqueStringDriverCode();
+            MinAdjacentSwapsToMakePalindromeDriverCode();
+            NumberOfGoodPairsDriverCode();
+        }
+
+        private static void NumberOfGoodPairsDriverCode()
+        {
+            NumberOfGoodPairs numberOfGoodPairs = new NumberOfGoodPairs();
+             numberOfGoodPairs.NumIdenticalPairs(new int[] { 2, 2, 1, 5, 1, 5, 5, 2, 3, 1, 1, 5, 3, 2, 3, 3, 3, 1, 3, 3, 4, 3, 1, 3, 1, 4, 5, 5, 2, 2, 1, 3, 5, 2, 2, 4, 3, 2, 5, 3, 1, 1, 3, 3, 2, 5, 2, 1, 2, 4, 3, 4, 4, 3, 2, 4, 4, 1, 3, 3, 3, 5, 5, 5, 4, 1, 1, 2, 3, 3, 2, 5, 3, 4, 5, 3, 1, 2, 5, 4, 5, 2, 3, 3, 1, 5, 2, 4, 2, 4, 4, 3, 1, 3});
+           // numberOfGoodPairs.NumIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 });
+        }
+
+        private static void MinAdjacentSwapsToMakePalindromeDriverCode()
+        {
+            MinAdjacentSwapsToMakePalindrome minAdjacentSwapsToMakePalindrome = new MinAdjacentSwapsToMakePalindrome();
+            minAdjacentSwapsToMakePalindrome.AdjacentSwaps("asflkj");
         }
 
         private static void MinDeletionUniqueStringDriverCode()
