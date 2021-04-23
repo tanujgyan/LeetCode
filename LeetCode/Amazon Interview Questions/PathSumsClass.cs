33d55c92a9b747d6e423d44cc550dbf79f5c8134
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-04-23 07:44:49 -0400

    recursion

--- LeetCode/Amazon Interview Questions/PathSumsClass.cs
+++ LeetCode/Amazon Interview Questions/PathSumsClass.cs
@@ -6,23 +6,66 @@ namespace LeetCode.Amazon_Interview_Questions
 {
     class PathSumsClass
     {
-
+        bool? targetAchieved=null;
         public bool HasPathSum(TreeNode root, int targetSum)
         {
             if (root == null)
             {
                 return false;
             }
-            else if(root.left==null && root.right==null && targetSum-root.val==0)
+            else if (root.left == null && root.right == null && targetSum - root.val == 0)
             {
                 return true;
             }
             else
             {
-                return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+                var a = HasPathSum(root.left, targetSum - root.val);
+                var b = HasPathSum(root.right, targetSum - root.val);
+                return a || b;
             }
         }
 
+        public bool HasPathSum2(TreeNode treeNode, int targetSum)
+        {
+
+            var result = RecursiveCalls(treeNode.left, treeNode.val, targetSum);
+
+            if (!result)
+                result = RecursiveCalls(treeNode.right, treeNode.val, targetSum);
+
+            return result;
+        }
+        public bool RecursiveCalls(TreeNode root, int sum, int targetSum)
+        {
+            if(targetAchieved!=null)
+            {
+                return (bool)targetAchieved;
+            }
+
+                if (root == null)
+                {
+                    return false;
+                }
+                sum += root.val;
+
+                if (root.left == null && root.right == null)
+                {
+                    if (sum == targetSum)
+                    {
+                        targetAchieved = true;
+                        return true;
+
+                    }
+
+
+                }
+
+                 RecursiveCalls(root.left, sum, targetSum);
+                    RecursiveCalls(root.right, sum, targetSum);
+            
+            return targetAchieved??false;
+           
+        }
 
     }
 
---
+++ LeetCode/Amazon Interview Questions/WordSearchClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Amazon_Interview_Questions
+{
+    class WordSearchClass
+    {
+        string s = "";
+        public void Exist(char[][] board, string word)
+        {
+           for(int i=0;i<board.Length;i++)
+            {
+                for(int j=0;j<board[i].Length;j++)
+                {
+                    if(board[i][j]==word[0])
+                    {
+                      var ret=  RecursiveMethod(board, word, i, j, 0);
+
+                    }
+                }
+            }
+
+        }
+        public bool RecursiveMethod(char[][] board, string word, int row, int column,int indexofnextletter)
+        {
+            bool ret = false;
+            if(row<0 || row>=board.Length || column<0 || column>=board[row].Length)
+            {
+                return false;
+            }
+            if (indexofnextletter < word.Length && board[row][column] == word[indexofnextletter])
+            {
+                ret = true;
+                s += word[indexofnextletter];
+            }
+            else
+            {
+                return ret;
+            }
+             ret= RecursiveMethod(board, word, row + 1, column, indexofnextletter+1)
+                || RecursiveMethod(board, word, row - 1, column, indexofnextletter+1)
+                || RecursiveMethod(board, word, row, column + 1, indexofnextletter+1)
+                || RecursiveMethod(board, word, row, column - 1, indexofnextletter+1);
+            
+            return ret;
+        }
+    }
+}
