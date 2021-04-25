e190cd2987860db88fe15abc4845433c33627da5
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-04-24 23:51:14 -0400

    https://leetcode.com/problems/last-stone-weight/

---
+++ LeetCode/Amazon Interview Questions/LastStoneWeightClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Amazon_Interview_Questions
+{
+    /// <summary>
+    /// https://leetcode.com/problems/last-stone-weight/
+    /// </summary>
+    class LastStoneWeightClass
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            
+            var stoneset = stones.ToList();
+            while (stoneset.Count > 1)
+            {
+                stoneset= stoneset.OrderBy(x => x).ToList();
+
+                //take difference of last two elements
+                var last = stoneset.ElementAt(stoneset.Count - 1);
+                var secondlast = stoneset.ElementAt(stoneset.Count - 2);
+                var diff = last - secondlast;
+                //remove last two elements
+                stoneset.Remove(last);
+                stoneset.Remove(secondlast);
+                //add the difference
+                stoneset.Add(diff);
+            }
+            //return the last left element
+            return stoneset.ElementAt(0);
+            
+        }
+    }
+}
