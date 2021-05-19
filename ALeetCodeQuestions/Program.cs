11df1a68953c2fb902359ce0de9d94ea34ac4d2a
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-05-19 18:27:16 -0400

    new problems
     https://leetcode.com/problems/reorder-data-in-log-files/

--- ALeetCodeQuestions/Program.cs
+++ ALeetCodeQuestions/Program.cs
@@ -10,6 +10,13 @@ namespace ALQ
         {
             //CombinationSumDriverCode();
             ItemsInContainerDriverCode();
+            ReorderDataInLogFilesDriverCode();
+        }
+
+        private static void ReorderDataInLogFilesDriverCode()
+        {
+            ReorderDataInLogFiles reorderDataInLogFiles = new ReorderDataInLogFiles();
+            reorderDataInLogFiles.ReorderLogFiles(new string[] {"6p tzwmh ige mc", "ns 566543603829", "ubd cujg j d yf", "ha6 1 938 376 5", "3yx 97 666 56 5", "d 84 34353 2249", "0 tllgmf qp znc", "s 1088746413789", "ys0 splqqxoflgx", "uhb rfrwt qzx r", "u lrvmdt ykmox", "ah4 4209164350", "rap 7729 8 125", "4 nivgc qo z i", "apx 814023338 8"});
         }
 
         private static void ItemsInContainerDriverCode()
