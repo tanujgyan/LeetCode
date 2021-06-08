827a866a51e6a1c6414d1f0affd8a5609684fac7
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-06-07 20:36:34 -0400

    max performance of team

---
+++ LeetCodeChallenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeChallenge.June_2021;
+using System;
+
+namespace LeetCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumPerformanceOfATeamDriverCode();
+        }
+
+        private static void MaximumPerformanceOfATeamDriverCode()
+        {
+            MaximumPerformanceOfATeam maximumPerformanceOfATeam = new MaximumPerformanceOfATeam();
+            maximumPerformanceOfATeam.MaxPerformance(6, new int[] { 2, 10, 3, 1, 5, 8 }, new int[] { 5, 4, 3, 9, 7, 2 }, 3);
+        }
+    }
+}
