3076f5e3714b5b450e5448696bb7575e2c407f8e
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-06-10 09:57:13 -0400

    binary tree dfs

--- Binary Tree/MaximumDepthOfBT.cs
+++ Binary Tree/MaximumDepthOfBT.cs
@@ -7,32 +7,30 @@ using System.Text;
 namespace Binary_Tree
 {
     /// <summary>
-    /// The algorithm here is the standard DFS and backtracking code with preorder traversal
+    /// The algorithm here is the standard DFS code with preorder traversal
     /// </summary>
     class MaximumDepthOfBT
     {
         private int maxDepth = 0;
         public int MaxDepth(TreeNode root)
         {
-            List<TreeNode> list = new List<TreeNode>();
-            MaxDepthHelper(root, list);
+            MaxDepthHelper(root, 0);
             return maxDepth;
 
         }
-        private void MaxDepthHelper(TreeNode root, List<TreeNode> list)
+        private void MaxDepthHelper(TreeNode root, int tempDepth)
         {
-            if(root==null)
+            if (root == null)
             {
                 return;
             }
-            list.Add(root);
-            if(root.left==null && root.right==null && list.Count>0)
+            tempDepth++;
+            if (root.left == null && root.right == null)
             {
-                maxDepth = Math.Max(list.Count, maxDepth);
+                maxDepth = Math.Max(tempDepth, maxDepth);
             }
-            MaxDepthHelper(root.left, list);
-            MaxDepthHelper(root.right, list);
-            list.RemoveAt(list.Count - 1);
+            MaxDepthHelper(root.left, tempDepth);
+            MaxDepthHelper(root.right, tempDepth);
         }
     }
 }
--- Binary Tree/Program.cs
+++ Binary Tree/Program.cs
@@ -13,7 +13,22 @@ namespace Binary_Tree
             //DistributeCoinsInBTDriverCode();
            // PrintAllPathsOfATreeDriverCode();
            // RecoverBSTDriverCode();
-            MaximumDepthOfBTDriverCode();
+            //MaximumDepthOfBTDriverCode();
+            BalancedBTDriverCode();
+        }
+
+        private static void BalancedBTDriverCode()
+        {
+            BalancedBT balancedBT = new BalancedBT();
+            TreeNode root = new TreeNode(1);
+           // root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+           
+            //root.left.right = new TreeNode(4);
+            //root.left.right.left = new TreeNode(7);
+            root.right.left = new TreeNode(5);
+            root.right.right = new TreeNode(6);
+            balancedBT.IsBalanced(root);
         }
 
         private static void MaximumDepthOfBTDriverCode()
